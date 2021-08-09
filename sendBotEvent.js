12efe0332cd32a836119f34588a428d006f81842
vickky06 <51980219+vickky06@users.noreply.github.com>
2021-08-09 18:58:31 +0530

    console changes

--- sendBotEvent.js
+++ sendBotEvent.js
@@ -2,8 +2,9 @@
     console.log('inside custom script')
     window.addEventListener('message', function (event1) {
         try {
+             console.log('EVENT RECEIVED', event1);
             let event = JSON.parse(event1.data);
-            console.log('EVENT RECEIVED', event);
+           
             console.log(event.event_code,"EVENT CODE");console.log(event.data.code,"DATA CODE");
            
             if ("custom-event" == event.event_code && "open_url" === event.data.code) {
